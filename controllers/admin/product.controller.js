b55afc67900c5c5cc4e61f5476262cf3ab834a1e
trantai3 <trantai09032003@gmail.com>
2024-06-13 11:44:57 +0700

    get data from products to display

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,13 @@
+const Product = require("../../models/product.model")
 // [GET] /admins/products
-module.exports.index = (req, res) => {   
+module.exports.index = async (req, res) => {   
+    const products = await Product.find({
+        deleted: false
+    })
+
+    console.log(products)
     res.render("admin/pages/products/index", { // send to client view and add database pageTitle
-        pageTitle: "Danh sách sản phẩm"
+        pageTitle: "Danh sách sản phẩm",
+        products: products
     });
 }

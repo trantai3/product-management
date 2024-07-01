758d524f858d4b6f55c355c95ec6ad49bb770bb7
trantai3 <trantai09032003@gmail.com>
2024-07-01 21:28:16 +0700

    preview image

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -126,3 +126,18 @@ if (showAlert) {
     }, time)
 }
 // End Show Alert
+
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]")
+if (uploadImage) {
+    const uploadImageInput = document.querySelector("[upload-image-input]")
+    const uploadImagePreview = document.querySelector("[upload-image-preview]")
+
+    uploadImageInput.addEventListener("change", (e) => {
+        const file = e.target.files[0]
+        if (file) {
+            uploadImagePreview.src = URL.createObjectURL(file)
+        }
+    })
+}
+

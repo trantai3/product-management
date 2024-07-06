7a84cd8ff5dadc596e1db491b455c8463588cccf
trantai3 <trantai09032003@gmail.com>
2024-07-06 10:55:10 +0700

    push static files to the Cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,14 +1,16 @@
-const express = require('express')  // import module 'express'
-const multer  = require('multer')
-const router = express.Router();   // initialize middleware router
-const storageMulter = require('../../helpers/storageMulter')
-const upload = multer({ storage: storageMulter() })
+const express = require('express') // import module 'express'
+const multer = require('multer')
+
+const router = express.Router(); // initialize middleware router
+
+const upload = multer()
 
 
 const controller = require('../../controllers/admin/product.controller') // import dashboard.controller file
 const validate = require('../../validates/admin/product.validate') // import validate file
 
-router.get('/', controller.index)   // define router handler with a path "/"
+const uploadCloud = require('../../middlewares/admin/uploadCloud.middleware')
+router.get('/', controller.index) // define router handler with a path "/"
 
 
 router.patch('/change-status/:status/:id', controller.changeStatus) // status var can change the value
@@ -20,15 +22,16 @@ router.delete('/delete/:id', controller.deleteItem)
 router.get('/create', controller.create)
 
 router.post(
-    '/create', 
+    '/create',
     upload.single('thumbnail'),
+    uploadCloud.upload,
     validate.createPost, // middleware 
     controller.createPost)
 
 router.get('/edit/:id', controller.edit)
 
 router.patch(
-    '/edit/:id', 
+    '/edit/:id',
     upload.single('thumbnail'),
     validate.createPost, // middleware 
     controller.editPatch)

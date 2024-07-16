5604ac0ab8e31d02a6ecf7c0e94639e6e35fad66
trantai3 <trantai09032003@gmail.com>
2024-07-16 11:47:04 +0700

    build product catalog management(2)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,25 +1,32 @@
 const ProductCategory = require('../../models/product-category.model')
 const systemConfig = require('../../config/system')
+const createTreeHelper = require('../../helpers/createTree')
 // [GET] /admins/products-category
 module.exports.index = async (req, res) => {
     const records = await ProductCategory.find({
         deleted: false
     })
+    const newRecords = createTreeHelper(records)
     res.render("admin/pages/products-category/index", { // send to client view and add database pageTitle
         pageTitle: "Danh mục sản phẩm",
-        records: records
+        records: newRecords
     })
 }
 
 // [GET] /admins/products-category/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+    const records = await ProductCategory.find({
+        deleted: false
+    })
+    const newRecords = createTreeHelper(records)
     res.render("admin/pages/products-category/create", { // send to client view and add database pageTitle
         pageTitle: "Tạo danh mục sản phẩm",
+        records: newRecords
     })
 }
 
 // [POST] /admins/products-category/create
-module.exports.createPost = async (req, res) => {
+module.exports.createPost = async (req, res) => {   
     if (req.body.position) {
         req.body.position = parseInt(req.body.position)
     } else {

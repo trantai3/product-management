90cdceeb09d9e19a47aacff7f5b043bee5fc0595
trantai3 <trantai09032003@gmail.com>
2024-07-29 20:43:14 +0700

    edit accounts(2)

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -2,7 +2,7 @@ const md5 = require('md5');
 const Account = require('../../models/account.model')
 const Role = require('../../models/role.model')
 const systemConfig = require("../../config/system")
-// [GET] /admins/accounts
+// [GET] /admin/accounts
 module.exports.index = async (req, res) => {   
     let find = {
         deleted: false
@@ -23,7 +23,7 @@ module.exports.index = async (req, res) => {
     });
 }
 
-// [GET] /admins/accounts/create
+// [GET] /admin/accounts/create
 module.exports.create = async (req, res) => {   
     const roles = await Role.find({
         deleted: false
@@ -34,7 +34,7 @@ module.exports.create = async (req, res) => {
     });
 }
 
-// [POST] /admins/accounts/create
+// [POST] /admin/accounts/create
 module.exports.createPost = async (req, res) => {   
     const emailExist = await Account.findOne({
         email: req.body.email,
@@ -49,4 +49,50 @@ module.exports.createPost = async (req, res) => {
         await record.save()
         res.redirect(`${systemConfig.prefixAdmin}/accounts`)
     }
+}
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+    let find = {
+        _id: req.params.id,
+        deleted: false
+    };
+    try {
+        const data = await Account.findOne(find)
+        const roles = await Role.find({
+            deleted: false
+        })
+        res.render("admin/pages/accounts/edit", {
+            pageTitle: "Chỉnh sửa tài khoản",
+            data: data,
+            roles: roles
+        })
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/accounts`)
+    }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id
+
+    const emailExist = await Account.findOne({
+        _id: { $ne: id}, // tìm những bản ghi khác
+        email: req.body.email,
+        deleted: false
+    })
+
+    if (emailExist) {
+        req.flash("error", `Email ${req.body.email} đã tồn tại`)
+    } else {
+        if (req.body.password) {
+            req.body.password = md5(req.body.password)
+        } else {
+            delete req.body.password
+        }
+        
+        await Account.updateOne({ _id: id}, req.body)
+        req.flash("success", "Cập nhật tài khoản thành công!")
+    }
+    res.redirect("back")
 }
--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -26,3 +26,18 @@ module.exports.createPost = async (req, res, next) => {
   }
   next();
 }
+
+module.exports.editPatch = async (req, res, next) => {
+  if(!req.body.fullName) {
+    req.flash("error", "Vui lòng nhập họ tên!");
+    res.redirect("back");
+    return;
+  }
+
+  if(!req.body.email) {
+    req.flash("error", "Vui lòng nhập email!");
+    res.redirect("back");
+    return;
+  }
+  next();
+}

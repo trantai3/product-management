e9b44d5a9f130afbe2468040f2eb52729ab7d99d
trantai3 <trantai09032003@gmail.com>
2024-07-30 23:54:26 +0700

    add authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require('../../config/system')
 const Account = require('../../models/account.model')
+const Role = require('../../models/role.model')
 
 module.exports.requireAuth = async (req, res, next) => {
     if(!req.cookies.token) {
@@ -11,10 +12,15 @@ module.exports.requireAuth = async (req, res, next) => {
         token: req.cookies.token,
         deleted: false,
         status: "active"
-    })
-    if(!user)
+    }).select("-password")
+    if(!user) {
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
-    else {
+    } else { 
+        const role = await Role.findOne({
+            _id: user.role_id
+        }).select("title permissions")
+        res.locals.user = user
+        res.locals.role = role
         next()
     }
 }

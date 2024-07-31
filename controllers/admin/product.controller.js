e9aa7c58b6cd933b5867ce7c21af830041216365
trantai3 <trantai09032003@gmail.com>
2024-07-31 22:36:28 +0700

    add delete history

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -107,7 +107,17 @@ module.exports.changeMulti = async (req, res) => {
             break;
 
         case "delete-all":
-            await Product.updateMany({ _id: { $in: ids }}, {deleted: true, deletedAt: new Date()})
+            await Product.updateMany({ _id: { $in: ids }}, 
+                {
+                deleted: true, 
+                // deletedAt: new Date()
+                deletedBy: {
+                    account_id: res.locals.user.id,
+                    deletedAt: new Date()
+                }
+            })
+            req.flash("success", `Xóa thành công ${ids.length} sản phẩm!`)
+            break;
 
         case "change-position":
             for (const item of ids) {
@@ -120,7 +130,6 @@ module.exports.changeMulti = async (req, res) => {
             }
             break;
         default:
-
             break;
 
     }
@@ -134,7 +143,11 @@ module.exports.deleteItem = async (req, res) => {
     await Product.updateOne({_id : id},
     {
         deleted: true,
-        deletedAt: new Date()
+        // deletedAt: new Date()
+        deletedBy: {
+            account_id: res.locals.user.id,
+            deletedAt: new Date()
+        }
     })
     req.flash("success", "Xóa thành công!")
     res.redirect("back")
--- models/product.model.js
+++ models/product.model.js
@@ -32,7 +32,11 @@ const productSchema = new mongoose.Schema(
             type: Boolean,
             default: false
         },
-        deletedAt: Date,
+        // deletedAt: Date,
+        deletedBy: {
+            account_id: String,
+            deletedAt: Date 
+        },
     }, {
         timestamps: true
     }

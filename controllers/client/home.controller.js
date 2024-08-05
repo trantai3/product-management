8bef5ff0f1dc6dd5b9b9118ea4b142523b0931eb
trantai3 <trantai09032003@gmail.com>
2024-08-05 21:15:22 +0700

    display outstanding products list (client)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,17 @@
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
 // [GET] /
 module.exports.index = async (req, res) => {   
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active"
+  })
+  
+  const newProducts = productsHelper.priceNewProducts(productsFeatured)
+
   res.render("client/pages/home/index", { // send to client view and add database pageTitle
-    pageTitle: "Trang chủ"
+    pageTitle: "Trang chủ",
+    productsFeatured: newProducts
   });
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,18 +1,17 @@
 const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
+
 // // [GET] /products
 module.exports.index = async (req, res) => {   // add a property index 
     const products = await Product.find({  // handle the logics
       status: "active",
       deleted: false
     }).sort({ position: "desc" });  // get all
-    const newProduct = products.map(item => {
-        item.newPrice = (item.price*(100 - item.discountPercentage)/100).toFixed(0)
-        return item
-    })
+    const newProducts = productsHelper.priceNewProducts(products)
 
     res.render("client/pages/products/index", {  // send to client view 
       pageTitle: "Danh sách sản phẩm",
-      products: newProduct               
+      products: newProducts
     })
 }
 

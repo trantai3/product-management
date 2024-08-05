ed46dfca2106dd7cbcd2ffb8591d11d82f990283
trantai3 <trantai09032003@gmail.com>
2024-08-05 21:32:56 +0700

    display newest products list (client)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,16 +2,25 @@ const Product = require("../../models/product.model")
 const productsHelper = require("../../helpers/products")
 // [GET] /
 module.exports.index = async (req, res) => {   
+  // Start get outstanding products
   const productsFeatured = await Product.find({
     featured: "1",
     deleted: false,
     status: "active"
-  })
+  }).limit(6)
   
-  const newProducts = productsHelper.priceNewProducts(productsFeatured)
-
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured)
+  // End get outstanding products
+  // Start get newest products
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc" }).limit(6)
+  const newProductsNew = productsHelper.priceNewProducts(productsNew)
   res.render("client/pages/home/index", { // send to client view and add database pageTitle
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew
+
   });
 }

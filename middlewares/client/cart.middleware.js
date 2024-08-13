7cf6a41d7dc3ae9fe84f32de0653c20db84e83ac
trantai3 <trantai09032003@gmail.com>
2024-08-13 08:13:02 +0700

    create a mini cart button on the header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -10,6 +10,12 @@ module.exports.cartId = async (req, res, next) => {
         res.cookie("cartId", cart.id, {
             expires: new Date(Date.now() + expiresCookie)
         })
+    } else {
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        })
+        cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
+        res.locals.miniCart = cart
     }
     next()
 }

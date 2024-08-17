05480e57bac80db8ce2598d8f53b284f52c28d1d
trantai3 <trantai09032003@gmail.com>
2024-08-17 21:35:27 +0700

    when logging in, update cartId

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -70,11 +70,19 @@ module.exports.loginPost = async (req, res) => {
         return
     }
 
-    await Cart.updateOne({
-        _id: req.cookies.cartId
-    }, {
+    const cart = await Cart.findOne({
         user_id: user.id
     })
+
+    if (cart) {
+        res.cookie("cartId", cart.id)
+    } else {
+        await Cart.updateOne({
+            _id: req.cookies.cartId
+        }, {
+            user_id: user.id
+        })
+    }
     res.cookie("tokenUser", user.tokenUser)
     res.redirect("/")
 }
@@ -82,6 +90,7 @@ module.exports.loginPost = async (req, res) => {
 // [GET] /user/logout
 module.exports.logout = async (req, res) => {
     res.clearCookie("tokenUser")
+    res.clearCookie("cartId")
     res.redirect("/")
 }
 

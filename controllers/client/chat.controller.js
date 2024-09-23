9326adb77e4f1716998b4dffbef383d7efe76fbd
trantai3 <trantai09032003@gmail.com>
2024-09-23 17:44:47 +0700

    display data realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,6 +3,8 @@ const User = require('../../models/user.model')
 // [GET] /chat/
 module.exports.index = async (req, res) => {
     const userId = res.locals.user.id
+    const fullName = res.locals.user.fullName
+
     // SocketIO
     _io.once('connection', (socket) => {
         socket.on("CLIENT_SEND_MESSAGE", async (content) => {
@@ -12,6 +14,13 @@ module.exports.index = async (req, res) => {
                 content: content
             })
             await chat.save()
+
+            // Trả data về client
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: content
+            })
         })
       })
     // End SocketIO
--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,33 @@ if (formSendData) {
     })
 }
 // END CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+    const myId = document.querySelector("[my-id]").getAttribute("my-id")
+    const body = document.querySelector(".chat .inner-body")
+    const div = document.createElement("div")
+
+    let htmlFullName = ""
+    if (myId == data.userId) {
+        div.classList.add("inner-outgoing")    
+    } else {
+        htmlFullName = `<div class="inner-name">${data.fullName}</div>`
+        div.classList.add("inner-incoming")
+    }
+    div.innerHTML = `
+      ${htmlFullName}
+      <div class="inner-content">${data.content}</div>
+    `
+    body.appendChild(div)
+
+    body.scrollTop = body.scrollHeight
+})
+// END SERVER_RETURN_MESSAGE
+
+// Scroll Chat to Bottom
+const bodyChat = document.querySelector(".chat .inner-body")
+if (bodyChat) {
+    bodyChat.scrollTop = bodyChat.scrollHeight // thuộc tính của DOM
+}
+// End Scroll Chat to Bottom

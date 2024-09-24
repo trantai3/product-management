cb390b1ad52e2b8d603a98f12d28c77061ba82bf
trantai3 <trantai09032003@gmail.com>
2024-09-24 14:57:50 +0700

    typing(2)

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,6 +8,7 @@ if (formSendData) {
         if (content) {
             socket.emit("CLIENT_SEND_MESSAGE", content)
             e.target.elements.content.value = ""
+            socket.emit("CLIENT_SEND_TYPING", "hidden")
         }
     })
 }
@@ -17,6 +18,7 @@ if (formSendData) {
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const myId = document.querySelector("[my-id]").getAttribute("my-id")
     const body = document.querySelector(".chat .inner-body")
+    const boxTyping = document.querySelector(".chat .inner-list-typing")
     const div = document.createElement("div")
 
     let htmlFullName = ""
@@ -30,7 +32,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
       ${htmlFullName}
       <div class="inner-content">${data.content}</div>
     `
-    body.appendChild(div)
+    body.insertBefore(div, boxTyping)
 
     body.scrollTop = body.scrollHeight
 })
@@ -56,6 +58,20 @@ if (buttonIcon) {
 }
 // End Show Popup
 
+// Show Typing
+let timeOut
+const showTyping = () => {
+    socket.emit("CLIENT_SEND_TYPING", "show")
+
+    clearTimeout(timeOut)
+
+    timeOut = setTimeout(() => {
+      socket.emit("CLIENT_SEND_TYPING", "hidden")
+    }, 3000)
+}
+
+// End Show Typing
+
 // Insert Icon To Input
 const emojiPicker = document.querySelector('emoji-picker')
 if (emojiPicker) {
@@ -63,11 +79,16 @@ if (emojiPicker) {
     emojiPicker.addEventListener('emoji-click', (event) => {
         const icon = event.detail.unicode
         inputChat.value = inputChat.value + icon
+
+        const end = inputChat.value.length
+        inputChat.setSelectionRange(end, end)
+        inputChat.focus()
+        showTyping()
     })
 
     // Input Keyup
     inputChat.addEventListener("keyup", () => {
-        socket.emit("CLIENT_SEND_TYPING", "show")
+        showTyping()
     })
     // End Input Keyup
 }
@@ -75,7 +96,34 @@ if (emojiPicker) {
 // End Show Icon chat
 
 // SERVER_RETURN_TYPING
-socket.on("SERVER_RETURN_TYPING", (data) => {
-    console.log(data)
-})
+const elementListTyping = document.querySelector(".chat .inner-list-typing")
+
+if (elementListTyping) {
+    socket.on("SERVER_RETURN_TYPING", (data) => {
+        if (data.type == "show") {
+            const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`)
+            if(!existTyping) {
+                const boxTyping = document.createElement("div")
+                boxTyping.classList.add("box-typing")
+                boxTyping.setAttribute("user-id", data.userId)
+    
+                boxTyping.innerHTML = `
+                  <div class="inner-name">${data.fullName}</div>
+                  <div class="inner-dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                `
+                elementListTyping.appendChild(boxTyping)
+                bodyChat.scrollTop = bodyChat.scrollHeight
+            }
+        } else {
+            const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.userId}"]`)
+            if (boxTypingRemove) {
+                elementListTyping.removeChild(boxTypingRemove)
+            }
+        }
+    })
+}
 // END SERVER_RETURN_TYPING
